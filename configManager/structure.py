4a3af93fbc147e807e71217fd31015faf1989366
tomek <tomek@9.164.180.44>
2015-11-26 10:54:38 +0100

    .

--- configManager/structure.py
+++ configManager/structure.py
@@ -3,19 +3,19 @@ config = dict(
     tasks = dict(
 	sort_series = dict(
 	    disable = ["retry_failed", "seen", "seen_info_hash"],
-            thetvdb_lookup = "yes",
-            metainfo_series = "yes",
+            thetvdb_lookup = True,
+            metainfo_series = True,
             filesystem = dict(
                 path = "/opt/complete",
                 regexp = ".*\.(avi|mkv|mp4)$",
-                recurisve = "yes"
+                recursive = True
             ),
             seen = "local",
             regexp = dict(
                 reject = ["sample"]
             ),
-            require_filed = "series_name",
-            accept_all = "yes",
+            require_field = "series_name",
+            accept_all = True,
             move = dict(
                 to = "/mnt/storage/video/seriale/{{series_name}}/Season {{series_season|pad(2)}}",
                 filename = "{{series_name}} - {{series_id}}",
